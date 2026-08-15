0daad67c69046b7494f360c67b442d526a6c79bc
cabled510 <cafcem234@gmail.com>
2026-08-15 18:25:11 +0000

    Rename run button key in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -245,11 +245,11 @@ st.markdown("""
         margin-top: 15px;
     }
 
-     div.stKey-run_button button {
+     div.stKey-run2_button button {
         background-color: #0A2B7A;
         color: white;
     }
-    div.stKey-run_button button:hover {
+    div.stKey-run2_button button:hover {
         background-color: #6d28d9;
         color: white;
     }
@@ -349,7 +349,7 @@ elif st.session_state['page'] == 'Predict':
         alive_feb14 = st.toggle("Alive at Feb 14", value=True)
         alive_feb21 = st.toggle("Alive at Feb 21", value=False)
 
-        st.button("Run Prediction", key="run_button", use_container_width=True)
+        st.button("Run Prediction", key="run2_button", use_container_width=True)
 
     with col_right:
         st.markdown("""
