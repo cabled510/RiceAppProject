8124fd4d277d534e7f9f91472bff7c28c6b9cf2a
cabled510 <cafcem234@gmail.com>
2026-08-15 04:34:51 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -340,7 +340,7 @@ if st.session_state['page'] == 'Predict':
     
 with col_left:
     st.markdown("<div class='input-header'>Growth measurements</div>", unsafe_allow_html=True)
-
+    
     accession = st.selectbox("Accession", ["AGRA", "ADDO1", "GH10887", "Togbei"], index=0)
     treatment = st.selectbox("Treatment", ["Control", "Stress"], index=0)
 
