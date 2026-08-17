ca3effcd45d489f38e01574f6b609887358b0554
cabled510 <cafcem234@gmail.com>
2026-08-17 23:28:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -568,7 +568,7 @@ elif st.session_state['page'] == 'Batch':
 # --- DASHBOARD PAGE ---
 elif st.session_state['page'] == 'Dashboard':
     # 1. Top Metrics Summary Row (No numeric values)
-    left_space, m_col1, m_col2, m_col3, m_col4 = st.columns(4)
+    m_col1, m_col2, m_col3, m_col4 = st.columns(4)
 
     with m_col1:
         st.markdown("""
